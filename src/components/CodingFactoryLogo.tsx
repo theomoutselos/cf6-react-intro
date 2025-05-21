6bff4784921e6db76c8f574c91fa298114baf404
theomoutselos <teofilos2000@yahoo.gr>
2025-05-21 20:01:53 +0300

    feat: add cf logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,12 @@
+const CodingFactoryLogo = () =>{
+    return(
+        <>
+        <img
+            className="my-4 h-16"
+            src="/logo.png"
+            alt = "CF6"/>
+        </>
+    )
+
+}
+export default CodingFactoryLogo;
